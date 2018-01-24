25c40f53aaa58155b20d1b0b8ccb192fbd20653b
nghiepuit <nghiepuit@gmail.com>
2018-01-25 01:16:44 +0700

    Add Area Admin

--- NetCore-Shop/Startup.cs
+++ NetCore-Shop/Startup.cs
@@ -95,8 +95,16 @@ namespace NetCore_Shop
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Home}/{action=Index}/{id?}"
+                );
+
+                routes.MapRoute(
+                    name : "areaRoute",
+                    template : "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
+
             });
+
         }
     }
 }
